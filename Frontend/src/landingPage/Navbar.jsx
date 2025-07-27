448435ccf62f46c29d8ed3f7aee38ca1197e9a00
kapil <kapilkeer1506@gmail.com>
2025-07-27 22:56:48 +0530

    style anchar tag and improve css

--- Frontend/src/landingPage/Navbar.jsx
+++ Frontend/src/landingPage/Navbar.jsx
@@ -7,25 +7,23 @@ function Navbar() {
     <nav class="navbar navbar-expand-lg  p-3 border-bottom " style={{background:"white"}}>
   <div class="container">
    <a href="#"> <img src="media/images/logo.svg" alt="Logo" style={{width:"30%",marginLeft:"25px"}} /></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item mx-4">
-          <a class="nav-link active" aria-current="page" href="#">Signup</a>
+          <a className="nav-link change "  href="#">Signup</a>
         </li>
          <li class="nav-item mx-4">
-          <a class="nav-link active" aria-current="page" href="#">About</a>
+          <a className="nav-link change "  href="#">About</a>
         </li>
          <li class="nav-item mx-4">
-          <a class="nav-link active" aria-current="page" href="#">Product</a>
+          <a className="nav-link change "  href="#">Product</a>
         </li>
          <li class="nav-item mx-4">
-          <a class="nav-link active" aria-current="page" href="#">Pricing</a>
+          <a className="nav-link change"  href="#">Pricing</a>
         </li>
        <li class="nav-item mx-4">
-          <a class="nav-link active" aria-current="page" href="#">Support</a>
+          <a className="nav-link change"  href="#">Support</a>
         </li>
        
        
