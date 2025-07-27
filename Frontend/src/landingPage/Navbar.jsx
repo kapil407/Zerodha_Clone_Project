d4299a7207c2c6c089cf1612a8684b3d3bd9adb7
kapil <kapilkeer1506@gmail.com>
2025-07-27 19:09:57 +0530

    created Navbar and Open Account component

--- Frontend/src/landingPage/Navbar.jsx
+++ Frontend/src/landingPage/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+//largestBroker.svg
 
 function Navbar() {
   return (
-    <div></div>
+    
+    <nav class="navbar navbar-expand-lg  p-3 border-bottom " style={{background:"white"}}>
+  <div class="container">
+   <a href="#"> <img src="media/images/logo.svg" alt="Logo" style={{width:"30%",marginLeft:"25px"}} /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item mx-4">
+          <a class="nav-link active" aria-current="page" href="#">Signup</a>
+        </li>
+         <li class="nav-item mx-4">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+         <li class="nav-item mx-4">
+          <a class="nav-link active" aria-current="page" href="#">Product</a>
+        </li>
+         <li class="nav-item mx-4">
+          <a class="nav-link active" aria-current="page" href="#">Pricing</a>
+        </li>
+       <li class="nav-item mx-4">
+          <a class="nav-link active" aria-current="page" href="#">Support</a>
+        </li>
+       
+       
+      </ul>
+    </div>
+  </div>
+</nav>
   )
 }
 
--- Frontend/src/landingPage/home/Award.jsx
+++ Frontend/src/landingPage/home/Award.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function Award() {
   return (
-    <div>Award</div>
+    <div className='container'>
+      <div className='row'>
+      <div className='col-6'>
+        <img src="media/images/largestBroker.svg" alt="#" />
+      </div>
+      <div className='col-6'>
+        <h1 >Largest stock broker in India</h1>
+        <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+        <div className='row '>
+          <div className='col-6'>
+            <ul>
+              <li  className='mb-3'>Future and Options</li>
+              <li  className='mb-3'>Commodity derivatives</li>
+              <li  className='mb-3'>Currency derivatives</li>
+            </ul>
+          </div>
+          <div className='col-6'>
+             <ul>
+              <li  className='mb-3'>
+              Stock & IPOs</li>
+              <li  className='mb-3'>Direct mutual funds</li>
+              <li  className='mb-3'>Bonds and Govt. Securities</li>
+            </ul>
+          </div>
+        </div>
+         <img className='my-5' src="media/images/pressLogos.png" style={{width:"85%"}} alt="img" />
+      </div>
+     
+      </div>
+    </div>
   )
 }
 
