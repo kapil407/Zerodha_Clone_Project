5ef8ac990c606e45b1343eb8d3fc3e8d0fa573f7
kapil <kapilkeer1506@gmail.com>
2025-07-28 15:02:33 +0530

    Creates Not found component if user click invalid link it make user  demonstrate  Not found

---
+++ Frontend/src/landingPage/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='container mb-5 p-5 mt-5'>
+     <div className='row text-center'>
+        <div className='col-12 '>
+        <h1>404 Not Found</h1>
+        <p>Sorry , the page you are looking for does not exist.</p>
+    </div>
+     </div>
+    </div>
+  );
+}
+
+export default NotFound
