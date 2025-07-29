856ef557020e6a95fcd674ec20bc811db02880ed
kapil <kapilkeer1506@gmail.com>
2025-07-29 15:29:19 +0530

    created Support component with style

--- Frontend/src/landingPage/support/CreateTicket.jsx
+++ Frontend/src/landingPage/support/CreateTicket.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
-
+import { BsWalletFill } from "react-icons/bs";
 function CreateTicket() {
   return (
-    <div>CreateTicket</div>
+    <div className='container mt-5'>
+    <div className='row text-muted'>
+      <div className='col-4 mb-5'>
+          <h1  className='fs-4 mb-5'> <i class="fa-solid fa-circle-plus "></i> Account Opening</h1>
+          <a href="" className='mt-2 d-block' style={{textDecoration:"none"}}>Online Account Opening </a> <br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Offline Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Company , Partnership and HUF Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>NRI Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Zerodha IDFC First Bank 3-in-1 Account </a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Getting started</a>
+      </div>
+      <div className='col-4 mb-5'>
+         <h1 className='fs-4 mb-5'><i class="fa-solid fa-user"></i> Your Zerodha Account</h1>
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Login Credentials </a> <br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Account Modification and Segment Addition</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>DP ID and bank details</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Your Profile</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Transfer and conversion of shares </a><br />
+        
+      </div>
+        <div className='col-4 mb-5'>
+         <h1 className='fs-4 mb-5'><i class="fa-solid fa-chart-column"></i> Your Zerodha Account</h1>
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Margin/leverage Product and Order types</a> <br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Kite Web and Mobile</a><br />
+          <a href="" style={{textDecoration:"none"}}>Trading FAQs</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Corporate Actions</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Sentine</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Kite API</a>
+            <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Pi and others platforms</a>
+              <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Stockreports+</a>
+                <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>GTT</a>
+      </div>
+    </div>
+      <div className='row text-muted'>
+      <div className='col-4 mb-5'>
+          <h1  className='fs-4 mb-5'><BsWalletFill/> Funds</h1>
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Online Account Opening </a> <br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Offline Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Company , Partnership and HUF Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>NRI Account Opening</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Zerodha IDFC First Bank 3-in-1 Account </a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Getting started</a>
+      </div>
+      <div className='col-4 mb-5'>
+         <h1 className='fs-4 mb-5'><i class="fa-solid fa-circle-notch"></i> Console</h1>
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Login Credentials </a> <br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Account Modification and Segment Addition</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>DP ID and bank details</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Your Profile</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Transfer and conversion of shares </a><br />
+        
+      </div>
+        <div className='col-4 mb-5'>
+         <h1 className='fs-4 mb-5'><i class="fa-regular fa-circle"></i> Coin</h1>
+          <a href="" style={{textDecoration:"none"}}>Margin/leverage Product and Order types</a> <br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Kite Web and Mobile</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Trading FAQs</a><br />
+          <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Corporate Actions</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Sentine</a><br />
+          <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Kite API</a>
+            <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>Pi and others platforms</a>
+              <a href=""  className='mb-2 d-block' style={{textDecoration:"none"}}>Stockreports+</a>
+                <a href="" className='mb-2 d-block'  style={{textDecoration:"none"}}>GTT</a>
+      </div>
+    </div>
+    </div>
   )
 }
 
