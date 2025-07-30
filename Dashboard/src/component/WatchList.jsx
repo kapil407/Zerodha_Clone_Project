7e5d2c8631b4552d2b0ef00ed85906d7977cdf1e
kapil <kapilkeer1506@gmail.com>
2025-07-30 15:45:38 +0530

    created Holding and watchlist componenets

--- Dashboard/src/component/WatchList.jsx
+++ Dashboard/src/component/WatchList.jsx
@@ -4,16 +4,16 @@ import React, { useState, useContext } from "react";
 
 // import GeneralContext from "./GeneralContext";
 
-// import { Tooltip, Grow } from "@mui/material";
+import { Tooltip, Grow } from "@mui/material";
 
-// import {
-//   BarChartOutlined,
-//   KeyboardArrowDown,
-//   KeyboardArrowUp,
-//   MoreHoriz,
-// } from "@mui/icons-material";
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
 
-// import { watchlist } from "../data/data";
+import { watchlist } from "../data/data";
 // import { DoughnutChart } from "./DoughnoutChart";
 
 // const labels = watchlist.map((subArray) => subArray["name"]);
@@ -83,13 +83,13 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        {/* <span className="counts"> {watchlist.length} / 50</span> */}
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
       <ul className="list">
-        {/* {watchlist.map((stock, index) => {
+        {watchlist.map((stock, index) => {
           return <WatchListItem stock={stock} key={index} />;
-        })} */}
+        })}
       </ul>
 
       {/* <DoughnutChart data={data} /> */}
@@ -99,79 +99,79 @@ const WatchList = () => {
 
 export default WatchList;
 
-// const WatchListItem = ({ stock }) => {
-  // const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
 
-//   const handleMouseEnter = (e) => {
-//     setShowWatchlistActions(true);
-//   };
+  const handleMouseEnter = (e) => {
+    setShowWatchlistActions(true);
+  };
 
-//   const handleMouseLeave = (e) => {
-//     setShowWatchlistActions(false);
-//   };
+  const handleMouseLeave = (e) => {
+    setShowWatchlistActions(false);
+  };
 
-//   return (
-//     <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-//       <div className="item">
-//         <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
-//         <div className="itemInfo">
-//           <span className="percent">{stock.percent}</span>
-//           {stock.isDown ? (
-//             <KeyboardArrowDown className="down" />
-//           ) : (
-//             <KeyboardArrowUp className="down" />
-//           )}
-//           <span className="price">{stock.price}</span>
-//         </div>
-//       </div>
-//       {showWatchlistActions && <WatchListActions uid={stock.name} />}
-//     </li>
-//   );
-// };
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+    {showWatchlistActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
 
-// const WatchListActions = ({ uid }) => {
-//   const generalContext = useContext(GeneralContext);
+const WatchListActions = ({ uid }) => {
+  // const generalContext = useContext(GeneralContext);
 
-//   const handleBuyClick = () => {
-//     generalContext.openBuyWindow(uid);
-//   };
+  // const handleBuyClick = () => {
+  //   generalContext.openBuyWindow(uid);
+  // };
 
-//   return (
-//     <span className="actions">
-//       <span>
-//         <Tooltip
-//           title="Buy (B)"
-//           placement="top"
-//           arrow
-//           TransitionComponent={Grow}
-//           onClick={handleBuyClick}
-//         >
-//           <button className="buy">Buy</button>
-//         </Tooltip>
-//         <Tooltip
-//           title="Sell (S)"
-//           placement="top"
-//           arrow
-//           TransitionComponent={Grow}
-//         >
-//           <button className="sell">Sell</button>
-//         </Tooltip>
-//         <Tooltip
-//           title="Analytics (A)"
-//           placement="top"
-//           arrow
-//           TransitionComponent={Grow}
-//         >
-//           <button className="action">
-//             <BarChartOutlined className="icon" />
-//           </button>
-//         </Tooltip>
-//         <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
-//           <button className="action">
-//             <MoreHoriz className="icon" />
-//           </button>
-//         </Tooltip>
-//       </span>
-//     </span>
-//   );
-// };
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+         
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
